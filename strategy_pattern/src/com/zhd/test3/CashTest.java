918abf66e25af0bd4d35def83dd108b5a5e64dfb
TAIJI-zhanghd <srzm1213@163.com>
2019-07-11 19:29:39 +0800

    修改工厂测试类

--- strategy_pattern/src/com/zhd/test3/CashTest.java
+++ strategy_pattern/src/com/zhd/test3/CashTest.java
@@ -3,8 +3,8 @@ package com.zhd.test3;
 public class CashTest {
     public static void main(String[] args) {
         //测试返利 满300减100
-        CashSuper cs = new CashReturn("300","100");
-        double v = cs.acceptCash(200);
+        CashSuper cashSuper = CashFactory.instance("八折");
+        double v = cashSuper.acceptCash(200);
         System.out.println("应付"+v);
     }
 }
